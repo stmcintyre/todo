7c009541cffa2d2a903bc8ac5d72ccd8fca5ffde
Steven McIntyre <steven.mcintyre@injixo.com>
2014-07-28 16:50:28 +0100

    Add 'show active' functionality

--- js/router.js
+++ js/router.js
@@ -1,6 +1,8 @@
 Todos.Router.map(function () {
   this.resource('todos', { path: '/' }, function () {
     // additional child routes will go here later
+    this.route('active');
+    this.route('completed');
   });
 });
 
@@ -13,5 +15,16 @@ Todos.TodosRoute = Ember.Route.extend({
 Todos.TodosIndexRoute = Ember.Route.extend({
   model: function() {
     return this.modelFor('todos');
+  }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model: function(){
+    return this.store.filter('todo', function (todo) {
+      return !todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller){
+    this.render('todos/index', {controller: controller});
   }
 });
