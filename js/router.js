6e88359cd05ef77dcd5dddc80c0be6f586f5ee1d
Steven McIntyre <steven.mcintyre@injixo.com>
2014-07-28 16:51:14 +0100

    Add 'show completed' transitions

--- js/router.js
+++ js/router.js
@@ -28,3 +28,14 @@ Todos.TodosActiveRoute = Ember.Route.extend({
     this.render('todos/index', {controller: controller});
   }
 });
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+  model: function(){
+    return this.store.filter('todo', function (todo) {
+      return todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller){
+    this.render('todos/index', {controller: controller});
+  }
+});
