b98a09e0140bbe6408bca4ffd8e11d5c7e23e5df
Steven McIntyre <steven.mcintyre@injixo.com>
2014-07-28 16:58:08 +0100

    Add 'Clear all completed' button functionality

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -1,5 +1,10 @@
 Todos.TodosController = Ember.ArrayController.extend({
   actions: {
+    clearCompleted: function() {
+      var completed = this.filterBy('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
+    },
     createTodo: function() {
       // Get the todo title set by the "New Todo" text field
       var title = this.get('newTitle');
@@ -27,5 +32,13 @@ Todos.TodosController = Ember.ArrayController.extend({
   inflection: function () {
     var remaining = this.get('remaining');
     return remaining === 1 ? 'item' : 'items';
-  }.property('remaining')
+  }.property('remaining'),
+
+  hasCompleted: function() {
+    return this.get('completed') > 0;
+  }.property('completed'),
+
+  completed: function() {
+    return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted')
 });
